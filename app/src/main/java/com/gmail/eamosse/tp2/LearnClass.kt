fabe2eef8820cfc314e5423ef6f094481d3e46c3
Amosse Edouard <amosse.edouard@instant-system.com>
2022-03-17 09:49:29 +0100

    Initial commit

---
+++ app/src/main/java/com/gmail/eamosse/tp2/LearnClass.kt
@@ -0,0 +1,36 @@
+package com.gmail.eJohn.tp2
+
+//TODO Créer une classe Etudiant (name, code, sexe (M, F), adresse, age)
+
+
+fun listOf10(): List<Etudiant> {
+    TODO("Créer une liste de 10 étudiants")
+}
+
+fun sortAgeAscDesc(items: List<Etudiant>, type: String): List<Etudiant> {
+    TODO("Ordonner la liste par âge (croissant et décroissant)")
+}
+
+fun groupBySex(items: List<Etudiant>): Map<String, List<Etudiant>> {
+    TODO(Grouper les étudiants par sexe)
+}
+
+fun boysOrGirls(items: List<Etudiant>, sexe: String): List<Etudiant> {
+    TODO("Créer une liste contenant uniquement les filles ou les garçons\n")
+}
+
+fun onlyNames(items: List<Etudiant>): List<String> {
+    TODO("Créer une liste contenant uniquement le nom des étudiants")
+}
+
+fun deleteBySex(items: MutableList<Etudiant>, sexe: String) {
+    TODO("Supprimer tous les étudiants d’un sexe donné \n")
+}
+
+fun inverseList(items: MutableList<Etudiant>): List<Etudiant> {
+    TODO("Inverser la liste ")
+}
+
+fun emptyList(items: MutableList<Etudiant>) {
+    TODO("Vider la liste")
+}
